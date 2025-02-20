6d0b39ae9ce6a69583ed185651f31defe38f6a33
Denis Beck <denisbeck10@gmail.com>
2025-02-20 15:15:13 -0300

    feat: adiciona pop up resultado e melhora HTML, CSS e JS

--- script.js
+++ script.js
@@ -35,21 +35,32 @@ document.getElementById("quizForm").addEventListener("submit", function (event) 
     // Gera um feedback com base na pontuação total
     let mensagem = "";
     if (pontuacaoTotal >= 18) {
-        mensagem = "Parabéns, sua hidratação está ótima! Continue assim!";
+        mensagem = "Parabéns, sua hidratação está ótima!<br> Continue assim!";
     } else if (pontuacaoTotal >= 12) {
         mensagem = "Você está no caminho certo, mas pode melhorar sua hidratação.";
     } else {
         mensagem = "Cuidado! Você precisa se hidratar mais para evitar problemas de saúde.";
     }
 
-    // Exibe o resultado na tela
-    document.getElementById("resultado").innerHTML = `
-        <h3>Resultado do Quiz Hidrata Aí</h3>
+    // Exibe o resultado no popup
+    document.getElementById("popupResultado").innerHTML = `
         <p><strong>Nome:</strong> ${nome}</p>
         <p><strong>Idade:</strong> ${idade} anos</p>
         <p><strong>Conclusão:</strong> ${mensagem}</p>
     `;
 
-    // Limpa o formulário após exibir o resultado
-    document.getElementById("quizForm").reset();
+    // Exibe o modal e o overlay
+    document.getElementById("popup").classList.remove("hidden");
+    document.getElementById("overlay").classList.remove("hidden");
+
+    // Botão para fechar o popup
+    const fecharModalBtn = document.getElementById("fecharpopup");
+    if (fecharModalBtn) {
+        fecharModalBtn.addEventListener("click", function () {
+            document.getElementById("popup").classList.add("hidden");
+            document.getElementById("overlay").classList.add("hidden");
+            document.getElementById("quizForm").reset(); // Reseta o formulário ao fechar o modal
+        });
+    }
+
 });
